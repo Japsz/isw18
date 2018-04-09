f9fc91d7e34bfa175f3ac4592ce70b4c17592166
Japsz <benjamin.meneses.14@sansano.usm.cl>
2018-04-09 13:45:46 -0300

    puthon 1.1

--- python/tests/test_yqd.py
+++ python/tests/test_yqd.py
@@ -15,23 +15,31 @@ def load_quote(ticker,desde,hasta):
 	return yqd.load_yahoo_quote(ticker, desde, hasta)[1:-1]
 
 def simular(valor,r,sigma,restantes):
-    if restantes == 0:
+    if len(restantes) == 0:
         return valor
-    valor = r*valor + valor*sigma
-    return simular(valor,r,sigma,restantes - 1)
+    eps, restantes = restantes[0],restantes[1:]
+    valor = valor + r*valor + valor*sigma*eps
+    return simular(valor,r,sigma,restantes)
 
 def test(ticker,desde,hasta):
     # Download quote for stocks
     oparray = []
     clarray = []
+    len = 0
     for string in load_quote(ticker,desde,hasta):
         print(string)
+        len+=1
         array = string.split(",")
         oparray.append(float(array[1]))
         clarray.append(float(array[4]))
-    oparray = np.array(oparray)
-    print(np.std(oparray))
-
+    clarray = np.array(clarray)
+    sd = np.std(clarray)
+    sd = sd /(len - 1)
+    tot = 0
+    for i in range(0,10000):
+        tot += simular(clarray[-1],0.025,sd,np.random.normal(0,1,len-1))
+    tot = tot/10000
+    print(tot)
 
 
 if __name__ == '__main__':

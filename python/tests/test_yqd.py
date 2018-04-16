e924a6a0e7c5f4e4b630b510b1ef85c30571c765
Japsz <benjamin.meneses.14@sansano.usm.cl>
2018-04-16 18:58:13 -0300

    pequeños cambibos

--- python/tests/test_yqd.py
+++ python/tests/test_yqd.py
@@ -5,7 +5,6 @@ Created on Sat May 20 18:58:59 2017
 @author: c0redumb
 """
 import sys
-import sympy as sm
 import scipy
 import numpy as np
 
@@ -75,3 +74,4 @@ def test(ticker,desde,hasta):
 
 if __name__ == '__main__':
 	test(sys.argv[1],sys.argv[2],sys.argv[3])
+
